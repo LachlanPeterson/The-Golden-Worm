d5eeaca7e8502c513c13f485dbdd1964e01021ed
LachlanPeterson <14383@coderacademy.edu.au>
2023-05-18 19:23:02 +1000

    Play.py linting to Pep8

--- play.py
+++ play.py
@@ -4,7 +4,7 @@ import scores
 from pytimedinput import timedInput
 from random import randint
 from colorama import Fore, Style
- 
+
 # Set Board size
 board_width = 32
 board_height = 15
@@ -12,66 +12,73 @@ board_height = 15
 # Play Area
 play_area = (board_width - 2)*(board_height - 2)
 
-# Board positions based off size, no. of rows is determined by height and vice versa.
-board = [(row, column) for row in range(board_height) for column in range(board_width)]
+# Board positions based off size
+board = [
+    (row, col) for row in range(board_height) for col in range(board_width)
+    ]
 
 # Worm
-worm = [(board_height//2,6),(board_height//2,5),(board_height//2,4)]
-movement = {'up': (-1,0), 'left': (0,-1), 'down': (1,0), 'right': (0,1)}
+worm = [(board_height//2, 6), (board_height//2, 5), (board_height//2, 4)]
+movement = {'up': (-1, 0), 'left': (0, -1), 'down': (1, 0), 'right': (0, 1)}
 auto_move = movement['right']
-# Worm auto direction = right, so I call the key from above dict to get the value.
+# Worm auto direction = right, so I call the key from above dict to get value.
 
 # Game Over
 game_over = False
 
 # Set Score and max score
 score = 0
-max_score = play_area -len(worm)
+max_score = play_area - len(worm)
+
 
 # Gold Nuggets
 def generate_gold_position():
     global gold_position
-    gold_position = (randint(1, board_height - 2)), (randint(1, board_width -2))
-    # Regenerating gold position if its in worm 
+    gold_position = ((randint(1, board_height - 2)),
+                     (randint(1, board_width - 2)))
+
+    # Regenerating gold position if its in worm
     if gold_position in worm:
         generate_gold_position()
+
 
 # Print board function
 def print_board():
     for position in board:
         # Printing snake position
         if position == worm[0]:
-            print(Fore.MAGENTA + '0', end = '')
+            print(Fore.MAGENTA + '0', end='')
         elif position in worm[1:]:
-            print(Fore.MAGENTA + 'o', end = '')
-        # if position of row is a value of 0 or (board_height -1), or position of column is 0 or (board_height -1) then print border '#'
-        elif position[0] in (0, board_height - 1) or position[1] in (0, board_width - 1):
+            print(Fore.MAGENTA + 'o', end='')
+        # If position of row is a value of 0 or (board_height -1),
+        # or position of col is 0 or (board_height -1) then print border '#'
+        elif (position[0] in (0, board_height - 1) or
+              position[1] in (0, board_width - 1)):
             # printing border, need end as default is \n
-            print(Fore.GREEN + '@', end = '')
+            print(Fore.GREEN + '@', end='')
         # Checking if cell is equal to gold position
         elif position == gold_position:
-            print(Fore.YELLOW + 'G', end = '')
+            print(Fore.YELLOW + 'G', end='')
         else:
             # printing board space
-            print(' ', end = '')
-        
-        # Starting new line once you reach right column. 
-        if position[1] == board_width -1:
+            print(' ', end='')
+        # Starting new line once you reach right col.
+        if position[1] == board_width - 1:
             # Defaults new line.
             print('')
 
+
 def worm_movement():
-    # Referencing global score variable
     global score
     global max_score
     global game_over
-    # Worm head = first tuple in worm, so to move worm we are adding a new head = direciton + old head, then removing tail(last tuple).
+    # Worm head = first tuple in worm, so to move worm we are adding
+    # a new head = direciton + old head, then removing tail(last tuple).
     update_worm = worm[0][0] + auto_move[0], worm[0][1] + auto_move[1]
     # Inserting new head as first tuple in worm, meaning I have 4 tuples.
-    worm.insert(0,update_worm)
-    # You can see there are now 4 tuples with print(worm), so you need to pop last tuple in list.
+    worm.insert(0, update_worm)
 
-    # If worm eats gold, body grows and new gold is generated. 
+    # If worm eats gold, body grows and new gold is generated.
     if worm[0] == gold_position:
         score += 1
         if score == max_score:
@@ -81,15 +88,15 @@ def worm_movement():
             scores.update_highscores(score)
             print('You won and turned gold!')
             # worm_win()
-        else:    
+        else:
             generate_gold_position()
-    # If worm head is in: Itself, or the borders: Left & Right, Top & Bottom - respectively.
-    elif worm[0] in worm[1:] or worm[0][0] in (0, board_height - 1) or worm[0][1] in (0, board_width - 1):
+    # If worm head is in: Itself, or the borders: Left & Right, Top & Bottom.
+    elif (worm[0] in worm[1:] or worm[0][0] in (0, board_height - 1) or
+          worm[0][1] in (0, board_width - 1)):
         worm_die()
-         
     else:
         worm.pop(-1)
-        # If you print(worm) there are 3 tuples.
+
 
 def worm_die():
     global game_over
@@ -102,7 +109,7 @@ def worm_die():
     os.system('cls' if os.name == 'nt' else 'clear')
     print(menu.worm_death)
     print(menu.line_break)
-    print(f'          You ate {Fore.YELLOW}{score}{Fore.WHITE} Golden Nuggets!')
+    print(f'         You ate {Fore.YELLOW}{score}{Fore.WHITE} Golden Nuggets!')
     print(menu.line_break)
     print('  1. Retry')
     print('  2. Menu')
@@ -121,27 +128,30 @@ def worm_die():
             # Error Handling
             case _:
                 print(menu.line_break)
-                print(f" Please enter a valid input from the Menu:\n {Style.DIM}(Retry: '1' or 'Retry')\n (Menu: '2' or 'Menu'){Style.NORMAL}")
+                print(f" Please enter a valid input from the Menu:\n "
+                      f"{Style.DIM}(Retry: '1' or 'Retry')\n "
+                      f"(Menu: '2' or 'Menu'){Style.NORMAL}")
                 print(menu.line_break)
                 worm_die_input()
     # Run worm die input
     worm_die_input()
 
+
+# Resetting game
 def game_reset():
-    # Resetting game.
     global score
     global game_over
     global worm
     global auto_move
-    
     score = 0
     game_over = False
-    worm = [(board_height//2,6),(board_height//2,5),(board_height//2,4)]
+    worm = [(board_height//2, 6), (board_height//2, 5), (board_height//2, 4)]
     auto_move = movement['right']
+
 
 def game_play():
     global auto_move
-    
+
     # Reset Game
     game_reset()
 
@@ -149,7 +159,7 @@ def game_play():
     generate_gold_position()
 
     # While game is game over is False (game is playing) run:
-    while game_over == False:
+    while not game_over:
         # Reprint board, giving impression of movement
         os.system('cls' if os.name == 'nt' else 'clear')
 
@@ -158,9 +168,10 @@ def game_play():
 
         # Print Board, worm, gold and border
         print_board()
-        
+
         # Player input for worm movement and timer between inputs
-        player_input,_ = timedInput(Fore.WHITE +'Press Movement Keys: ', timeout = 0.2)
+        player_input, _ = timedInput(
+            Fore.WHITE + 'Press Movement Keys: ', timeout=0.2)
         match player_input:
             case 'w' | 'W':
                 auto_move = movement['up']
@@ -170,8 +181,9 @@ def game_play():
                 auto_move = movement['down']
             case 'd' | 'D':
                 auto_move = movement['right']
-            # Only movement keys update movement. Errors handled as no invalid keys move worm.
+            # Errors handled as no invalid keys update worm.
             case _:
                 pass
-        # Updating new worm position after user input before game reprint 
+
+        # Updating new worm position after user input before game reprint
         worm_movement()
